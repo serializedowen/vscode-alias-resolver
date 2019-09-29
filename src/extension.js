bd715d23c9235f9bf32d9a96178db8fca40dd615
serializedowen <wjh199455@gmail.com>
2019-09-29 17:23:59 +0800

    @ as workspaceRoot

--- src/extension.js
+++ src/extension.js
@@ -1,6 +1,14 @@
 const mapping = {
-  "@/": "/Users/OwenWang/Documents/alias-resolver/"
+  "@/": vscode => vscode.workspace.rootPath + "/"
 };
+
+const activatedLanguage = [
+  "javascript",
+  "typescript",
+  "javascriptreact",
+  "typescriptreact",
+  "vue"
+];
 
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
@@ -9,25 +17,6 @@ const vscode = require("vscode");
 // const symbol = require("./symbols");
 const definitionProviderFactory = require("./definitionProvider");
 const completionItemProviderFactory = require("./completionProvider");
-// const settings = require("./settings.json");
-
-const completionItemProvider = completionItemProviderFactory(mapping);
-
-const definitionProvider = definitionProviderFactory(mapping);
-
-// function languageCheck(document) {
-//   const { languageId } = document;
-
-//   if (languageId === "javascript" || "javascriptreact") {
-//     return symbol.js;
-//   } else if (languageId === "typescript") {
-//     return symbol.ts;
-//   }
-
-//   return "text/plain";
-// }
-
-const commands = [];
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -35,18 +24,36 @@ exports.activate = function(context) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log(
-    'Congratulations, your extension "helloworld-sample" is now active!'
+    'Congratulations, your extension "alias-resolver" is now active!'
   );
 
+  for (let key in mapping) {
+    if (Object.prototype.toString.call(mapping[key]) === "[object Function]") {
+      mapping[key] = mapping[key](vscode);
+    }
+  }
+
+  const completionItemProvider = completionItemProviderFactory(mapping);
+  const definitionProvider = definitionProviderFactory(mapping);
   // commands.push(
   //   vscode.commands.registerCommand("extension.testHook", () => {
   //     vscode.window.showInformationMessage("test!");
   //   })
   // );
+  const commands = [];
+  const hooks = [];
+
+  // hooks.push(
+  //   vscode.commands.registerCommand("extension.disable", () => {
+  //     commands.map(command => command());
+  //   })
+  // );
+
+  // hooks.push("extension.enable", () => {});
 
   commands.push(
     vscode.languages.registerCompletionItemProvider(
-      ["javascriptreact", "javascript"],
+      activatedLanguage,
       completionItemProvider,
       ["/"]
     )
@@ -54,7 +61,7 @@ exports.activate = function(context) {
 
   commands.push(
     vscode.languages.registerDefinitionProvider(
-      ["javascriptreact", "javascript"],
+      activatedLanguage,
       definitionProvider
     )
   );
@@ -62,7 +69,7 @@ exports.activate = function(context) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  commands.push(
+  hooks.push(
     vscode.commands.registerCommand("extension.helloWorld", () => {
       // The code you place here will be executed every time your command is executed
       // Display a message box to the user
@@ -71,9 +78,8 @@ exports.activate = function(context) {
   );
 
   commands.map(command => context.subscriptions.push(command));
+  hooks.map(hook => context.subscriptions.push(hook));
 };
 
 // this method is called when your extension is deactivated
-exports.deactivate = function(context) {
-  commands.map(command => con);
-};
+exports.deactivate = function() {};
